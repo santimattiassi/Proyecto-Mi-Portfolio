01602e481b2fa49363d748a85647cc088a99aa57
santiago <santiago.mattiassi@gmail.com>
2020-10-20 23:16:25 -0300

    Correccion de boton enviar del formulario

---
+++ enviar.php
@@ -0,0 +1,28 @@
+<?php
+header("Content-type: text/html;charset=\"utf-8\"");
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['asunto'];
+$empresa = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Asunto: " . $asunto . " \r\n";
+$mensaje .= "Teléfono: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $empresa . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'santiago.mattiassi@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+if (mail($para, $asunto, utf8_decode($mensaje), $header))
+echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+echo "<script type='text/javascript'>window.location.href='index.html';</script>";
+
+?>
